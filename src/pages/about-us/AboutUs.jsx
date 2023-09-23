6afca81e1c96cf18e44b498dbc03f69479030e62
abdelrahmanes <abdelrahmanelshershaby@gmail.com>
2023-09-23 14:31:40 +0300

    edit about us page coming soon

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,37 +1,11 @@
+import ComingSoon from "../../components/ComingSoon";
 import SubPageHero from "../../components/SubPageHero";
 import Layout from "../../layout";
 function AboutUs() {
   return (
     <Layout>
       <SubPageHero title={"About Us"} />
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
-      <p>About us</p>
+      <ComingSoon />
     </Layout>
   );
 }
